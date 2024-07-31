beb31e906547f6f7dca84e634ee9cc48b2debcb7
rayssassousa22 <rayssa.sousa78@outlook.com>
2024-07-31 19:46:33 +0000

    learningn about functions

--- funcoes/aula.js
+++ funcoes/aula.js
@@ -1,5 +1,22 @@
-function teste(seuNome){
-  console.log("teste " + seuNome);
-} //criando uma função
+function calcularJuros(valorInicial, modoDePagamento){
+  let valorFinal;
+  if (modoDePagamento === 'débito à vista'){
+    valorFinal = valorInicial - (valorInicial * 0.15);
+  } else if (modoDePagamento === 'pix' || modoDePagamento === 'dinheiro'){
+    valorFinal = valorInicial - (valorInicial * 0.2);
+  } else if (modoDePagamento === 'crédito 2 vezes'){
+    valorFinal = valorInicial;
+  } else if (modoDePagamento === 'crédito'){
+    valorFinal = valorInicial + (valorInicial * 0.1);
+  }
 
-teste('Rayssa'); //invocando uma fução no código
+  console.log(valorFinal);
+}
+
+function main(){
+  console.log('Programa principal');
+}
+
+main();
+
+calcularJuros(100, 'crédito');
