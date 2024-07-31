627f5c734feb1ef73923b4ede79e65e4df7484ae
rayssassousa22 <rayssa.sousa78@outlook.com>
2024-07-31 14:42:24 +0000

    learning about conditionals

--- condicionais/aula.js
+++ condicionais/aula.js
@@ -1,10 +1,19 @@
-const estaDeDia = true;
-const estaDeNoite = false;
+// const estaDeDia = true;
+//const estaDeNoite = false;
 
-let boolean = estaDeNoite;
+//let boolean = estaDeNoite;
 
-if (boolean == estaDeDia) {
-  console.log("Está de dia!");
-} else {
-  console.log("Está de noite");
+//if (boolean == estaDeDia) {
+  //console.log("Está de dia!");
+//} else {
+  //console.log("Está de noite");
+//}
+
+const numero = 30;
+const parOuImpar = numero % 2;
+
+if (parOuImpar == 0){
+  console.log("É par");
+} else{
+  console.log("É impar");
 }
