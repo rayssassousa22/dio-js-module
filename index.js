f2ef508b11c8579c140b20c7c7e0ab9afaa22e49
rayssassousa22 <rayssa.sousa78@outlook.com>
2024-07-31 14:26:21 +0000

    first challenge

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const precoCombustivel = 5.79;
 const kmPorLitro = 10;
-let distanciaEmKm = 100;
+let distanciaEmKm = 151;
 let litrosConsumidos = distanciaEmKm / kmPorLitro;
 let gastoMedio = litrosConsumidos * precoCombustivel; //diferente método mas era o mesmo raciocínio da outra
 
-console.log(gastoMedio);
+console.log(gastoMedio.toFixed(2)); //converte o valor que tem muitos numeros pra somente duas casas decimais
