8ac446289467f397cd6342f1fd18f6c19f989cc9
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-07-31 16:23:47 +0000

    learning condicionais

--- condicionais/aula.js
+++ condicionais/aula.js
@@ -4,16 +4,22 @@
 //let boolean = estaDeNoite;
 
 //if (boolean == estaDeDia) {
-  //console.log("Está de dia!");
+//console.log("Está de dia!");
 //} else {
-  //console.log("Está de noite");
+//console.log("Está de noite");
 //}
 
-const numero = 30;
-const parOuImpar = numero % 2;
+const numero = 0;
+const parOuImpar = (numero % 2) === 0;
 
-if (parOuImpar == 0){
+if (numero == 0) {
   console.log("É par");
-} else{
+} else if (parOuImpar) {
+  console.log("Número inválido");
+} else {
   console.log("É impar");
 }
+
+// = indica atribuição
+// == igualdade
+// === pra retornar de fato o valor verdadeiro (diferencia numero, string e etc)
