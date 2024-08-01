7b121f6f5c40b54bd4839fe087e3b3bf031892f7
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-01 14:46:30 +0000

    learning about objects and classes

--- objeto/aula.js
+++ objeto/aula.js
@@ -1,20 +1,24 @@
 class Pessoa {
   nome;
   idade;
+  localDeNascimento;
+  dataDeNascimento;
+
+  constructor (nome, idade, localDeNascimento){
+    this.nome = nome;
+    this.idade = idade;
+    this.localDeNascimento = localDeNascimento;
+    this.dataDeNascimento = 2024 - idade;
+  } //faz com que toda vez que esse código opere cobre do usuário seu nome e idade
 
   descrever(){
-    return ('Meu nome é ' + this.nome + ' e minha idade é ' + this.idade);
+    return ('Meu nome é ' + this.nome + ' e minha idade é ' + this.idade + '. Eu nasci em ' + this.localDeNascimento + ' no ano de ' + this.dataDeNascimento);
   }
-}
+} // uma classe define tipo uma estrutura base de um objeto e com base em uma classe podemos criar objetos invisíveis e atribuir valores para usá-los mais tarde
 
 
-const rayssa = new Pessoa();
-rayssa.nome = 'Rayssa da Silva Sousa';
-rayssa.idade = 18;
-
-const izabel = new Pessoa();
-izabel.nome = 'Izabel Moraes Cabalini';
-izabel.idade = 17;
+const rayssa = new Pessoa('Rayssa', 19, 'Bom Jesus do Itabapoana'); //com o constructor ele consegue receber parâmetros
+const izabel = new Pessoa('Izabel', 18, 'São José do Calçado');
 
 console.log(rayssa.descrever());
 console.log(izabel.descrever());
