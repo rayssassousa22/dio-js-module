40cb461bfaa1fa0b6369802bf5fda3eafd171861
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-01 14:35:54 +0000

    learning about objects and classes

--- objeto/aula.js
+++ objeto/aula.js
@@ -1,11 +1,20 @@
-const rayssa = {
-  nome: 'Rayssa da Silva Sousa',
-  idade: 18,
-  descrever: function(){
-    console.log('Meu nome é ' + this.nome + ' e minha idade é ' + this.idade); //usando funções dentro do objeto
+class Pessoa {
+  nome;
+  idade;
+
+  descrever(){
+    return ('Meu nome é ' + this.nome + ' e minha idade é ' + this.idade);
   }
-}; //um objeto é um local que agrupa informações diversas
+}
 
-const nome = 'nome';
 
-console.log(rayssa[nome]);
+const rayssa = new Pessoa();
+rayssa.nome = 'Rayssa da Silva Sousa';
+rayssa.idade = 18;
+
+const izabel = new Pessoa();
+izabel.nome = 'Izabel Moraes Cabalini';
+izabel.idade = 17;
+
+console.log(rayssa.descrever());
+console.log(izabel.descrever());
