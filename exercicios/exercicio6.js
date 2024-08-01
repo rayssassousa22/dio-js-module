a909730fd33e63e108a25790b469840e4c3a6ecf
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-01 19:09:56 +0000

    learning about arrays

--- exercicios/exercicio6.js
+++ exercicios/exercicio6.js
@@ -14,6 +14,20 @@ class Pessoa{
   }
 }
 
-const pessoa = new Pessoa('José', 70, 1.75);
+const pessoa = new Pessoa('José', 120, 1.75);
 
-console.log(pessoa.nome + ' tem o IMC de ' + pessoa.calcularImc().toFixed(2));
+function classificarImc(imc){
+  if (imc < 18.5){
+    return 'Abaixo do peso';
+  } else if ((imc >= 18.5 && imc < 25)){
+    return 'Peso Normal';
+  } else if ((imc >= 25 && imc < 30)){
+    return 'Acima do peso';
+  } else if ((imc >=30 && imc < 40)){
+    return 'Obeso';
+  } else {
+    return 'Obesidade grave';
+  }
+}
+
+console.log(pessoa.nome + ' tem o IMC de ' + pessoa.calcularImc().toFixed(2) + ' e está com ' + classificarImc(pessoa.calcularImc()));
