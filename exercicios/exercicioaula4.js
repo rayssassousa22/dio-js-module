53b030abb0ff0191dc29939d2cf41b87589d9f30
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-01 13:25:09 +0000

    fourth exercise

--- exercicios/exercicioaula4.js
+++ exercicios/exercicioaula4.js
@@ -1,5 +1,15 @@
-function meuNome(nome){
+function escrevaMeuNome(nome){
   console.log("Meu nome é: " + nome);
 }
 
-meuNome('Rayssa');
+escrevaMeuNome('Rayssa');
+
+function verificarIdade(idade){
+  if (idade < 18){
+    console.log('Você é menor de idade');
+  } else {
+    console.log('Você é de maior!');
+  }
+}
+
+verificarIdade(18);
