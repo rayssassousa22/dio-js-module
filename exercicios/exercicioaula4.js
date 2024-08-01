04a4b291fc3591986b4834697d1a390a0e4ca2fd
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-01 13:41:46 +0000

    fourth exercise

--- exercicios/exercicioaula4.js
+++ exercicios/exercicioaula4.js
@@ -1,15 +1,33 @@
-function escrevaMeuNome(nome){
-  console.log("Meu nome é: " + nome);
+/*function escrevaMeuNome(nome){
+  return ("Meu nome é: " + nome);
 }
-
-escrevaMeuNome('Rayssa');
 
 function verificarIdade(idade){
   if (idade < 18){
-    console.log('Você é menor de idade');
+    console.log(escrevaMeuNome('Rayssa') + '\nVocê é menor de idade');
   } else {
     console.log('Você é de maior!');
   }
 }
 
-verificarIdade(18);
+verificarIdade(17);*/
+
+const { X509Certificate } = require("crypto");
+
+function calcularJuros(precoNormal, formaDePagamento) {
+  if (formaDePagamento === 1) {
+    total = precoNormal - precoNormal * 0.1;
+    return total;
+  } else if (formaDePagamento === 2) {
+    total = precoNormal - precoNormal * 0.15;
+    return total;
+  } else if (formaDePagamento === 3) {
+    total = precoNormal;
+    return total;
+  } else {
+    total = precoNormal + precoNormal * 0.1;
+    return total;
+  }
+}
+
+console.log('Você irá pagar: ' + calcularJuros(50, 4));
