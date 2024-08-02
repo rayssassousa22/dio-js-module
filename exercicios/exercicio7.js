2618d5c84cfb9f5345a1c0739ebc27cb68bc69bd
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-02 11:49:45 +0000

    seventh exercise

---
+++ exercicios/exercicio7.js
@@ -0,0 +1,7 @@
+//Fazendo uma tabuada
+
+const numero = 2;
+
+for (let i = 1; i <= 10; i++){
+  console.log(numero + ' x ' + i + ' = ' + numero * i);
+} // o i age como o número da multiplicação da tabuada
