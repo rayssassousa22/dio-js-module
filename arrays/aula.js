58501d3b8c3b066f6b36bee9d1e533a64d28a4ea
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-02 11:05:15 +0000

    learningn about arrays

--- arrays/aula.js
+++ arrays/aula.js
@@ -1,7 +1,12 @@
 
 
-const alunos = ['joao', 'maria'];//arrays fazem um conjunto de dados similares
+const alunos = ['joao', 'maria'];
+//arrays fazem um conjunto de dados similares
 
-alunos.push('rayssa'); //adiciona um valor a array
+alunos.push('rayssa');// adiciona elementos a uma array
+
+alunos.pop(); //tira o último elemento da array original
+
+console.log(alunos.pop());
 
 console.log(alunos);
