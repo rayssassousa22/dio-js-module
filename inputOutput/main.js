cac723578c73845d9d38eadc89870621f6959d8d
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-02 13:55:11 +0000

    learning about input, importacoes e exportacoes de funcoes

--- inputOutput/main.js
+++ inputOutput/main.js
@@ -1,1 +1,11 @@
-const funcoes = require('./aula'); //recebe (importa) as funções que estão sendo exportadas
+const { get, print } = require('./aula'); //recebe (importa) as funções que estão sendo exportadas, estabelece uma conexão
+
+const pessoa = {
+  nome: 'rayssa'
+};
+
+const { nome } = pessoa;
+
+print(get());
+print(nome);
+//seria como se fizesse const nome = pessoa.nome;
