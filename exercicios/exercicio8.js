6b3d34b79fbb6ac2d78b39abd1e0f37a967fe193
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-09 13:37:38 +0000

    desafio dois

--- exercicios/exercicio8.js
+++ exercicios/exercicio8.js
@@ -1,21 +1,26 @@
 const { gets, print } = require("./main.js");
 
-//criando as listas para usar os numeros
+
 const n = gets(); 
 
-/* O primeiro número quando você chama define o número de vezes que ele irá rodar!! */
-let maiorNumeroPar = 0;
-let menorNumeroImpar = 0;
+
+let maiorNumeroPar = null;
+let menorNumeroImpar = null;
 
 for (let i = 0; i < n; i++) {
   const lista = gets(); //imprime todas de uma vez só
-  if (lista % 2 === 0) { //ele percorre todos os números pares de uma vez
-    if(lista > maiorNumeroPar){
+
+  
+  if (lista % 2 === 0) {
+    if(maiorNumeroPar === null){
+      maiorNumeroPar = lista;
+    } else if(lista > maiorNumeroPar){
       maiorNumeroPar = lista;
     }
+    
   } else { //também percorre todos os números impares
-    if(menorNumeroImpar === 0){
-      menorNumeroImpar = lista; //coloca o primeiro numero impar encontrado na lista
+    if(menorNumeroImpar === null){
+      menorNumeroImpar = lista;
     } else if (lista < menorNumeroImpar){
       menorNumeroImpar = lista; //descobre de fato o menor numero impar
     }
