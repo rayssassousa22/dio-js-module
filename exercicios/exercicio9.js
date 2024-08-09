65b72b7ab7e320de4ec384e6e42c86c11a0751b7
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-09 16:21:06 +0000

    exercicio nove

---
+++ exercicios/exercicio9.js
@@ -0,0 +1,30 @@
+const { gets, print } = require("./main.js");
+
+const salarioValorBruto = 1320;
+const beneficiosAdicionais = 800;
+
+//aliquotas 
+//   0.00 a 1100.00 = 5%
+//   1100.01 a 2500.00 = 10%
+//   + 2500.00 = 15%
+
+let percentualImpostoSalarial = 0;
+
+percentualSobreSalario(salarioValorBruto);
+
+let resultado = (salarioValorBruto - (salarioValorBruto * percentualImpostoSalarial)) + beneficiosAdicionais;
+
+print("O salário líquido do funcionário é de R$" + resultado.toFixed(2));
+
+function percentualSobreSalario(salario){
+  if(salario > 0.00 && salario <= 1100.00){
+    percentualImpostoSalarial = 0.05;
+    return percentualImpostoSalarial;
+  } else if (salario >=1100.01 && salario <= 2500.00){
+    percentualImpostoSalarial = 0.1;
+    return percentualImpostoSalarial;
+  } else if (salario > 2500.00){
+    percentualImpostoSalarial = 0.15;
+    return percentualImpostoSalarial;
+  }
+}
