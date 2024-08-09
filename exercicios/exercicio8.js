921e0fd07216d50c69ac7b6b4492af3aa9f18af4
Rayssa Sousa <35257480-rayssasousa22@users.noreply.replit.com>
2024-08-09 12:54:15 +0000

    desafio dois outra maneira de resoluação

--- exercicios/exercicio8.js
+++ exercicios/exercicio8.js
@@ -1,39 +1,26 @@
-const { get, print } = require("./main.js");
+const { gets, print } = require("./main.js");
 
 //criando as listas para usar os numeros
-const lista = [];
-const numerosPares = [];
-const numerosImpares = [];
+const n = gets(); 
+
+/* O primeiro número quando você chama define o número de vezes que ele irá rodar!! */
+let maiorNumeroPar = 0;
+let menorNumeroImpar = 1;
+/*const numerosPares = [];
+const numerosImpares = [];*/
 
 //vai colocar os inputs em uma lista e comparar se é par ou ímpar
-for (let i = 0; i < 7; i++) {
-  lista[i] = get();
-  if (lista[i] % 2 == 0) {
-    numerosPares.push(lista[i]);
+for (let i = 0; i < n; i++) {
+  const lista = gets();
+  if (lista % 2 === 0) {
+    if(lista > maiorNumeroPar){
+      maiorNumeroPar = lista;
+    }
   } else {
-    numerosImpares.push(lista[i]);
+    if(lista <= menorNumeroImpar){
+      menorNumeroImpar = lista;
+    }
   }
 }
 
-//vai chegar a condição do desafio
-function menorEhMaior(pares, impares) {
-  let maior = pares[0];
-  let menor = impares[0];
-
-  for (let count = 0; count < pares.length; count++) {
-    if (pares[count] > maior) {
-      maior = pares[count];
-    }
-  }
-
-  print(maior);
-
-  for (let c = 0; c < impares.length; c++) {
-    if (impares[c] < menor) {
-      menor = impares[c];
-    }
-  }
-
-  print(menor);
-}
-menorEhMaior(numerosPares, numerosImpares);
+print(maiorNumeroPar + " " + menorNumeroImpar);
--- exercicios/main.js
+++ exercicios/main.js
@@ -1,7 +1,7 @@
-const armario = [5, 3, 4, 17, 10, 8, 1];
+const armario = [6, 3, 4, 17, 10, 8, 1];
 let index = 0;
 
-function get() {
+function gets() {
   let numeroDoArmario = armario[index];
   index++;
   return numeroDoArmario;
@@ -11,4 +11,4 @@ function print(texto) {
   console.log(texto);
 } //printa na tela esse input
 
-module.exports = { get, print };
+module.exports = { gets, print };
